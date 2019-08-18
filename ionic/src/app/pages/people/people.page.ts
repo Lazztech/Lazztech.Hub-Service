d606dcbbcf767745797a13ebdbb9c2a2c31271ad
Gian Lazzarini <gianlazzarini@gmail.com>
2019-08-18 12:32:43 -0700

    Implementing front end for people page.

--- ionic/src/app/pages/people/people.page.ts
+++ ionic/src/app/pages/people/people.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-people',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeoplePage implements OnInit {
 
-  constructor() { }
+  private apollo: Apollo;
+  persons: [] = [];
+
+  constructor(apollo: Apollo) {
+    this.apollo = apollo;
+  }
 
   ngOnInit() {
+    this.apollo.query({
+      query: gql`
+        query {
+          getAllPersons {
+            id
+            name
+            images {
+              id
+              image
+              personDescriptors {
+                id
+                descriptor
+              }
+            }
+          }
+        }
+      `
+    }).subscribe(({data}) => {
+      this.persons = data['getAllPersons'];
+      console.log(JSON.stringify(this.persons));
+      // console.log(this.persons[0].images[0].image);
+    });
   }
 
 }
