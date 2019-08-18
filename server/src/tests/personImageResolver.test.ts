cdb9feea53e9694f236c5d60976caf864e08fa14
Gian Lazzarini <gianlazzarini@gmail.com>
2019-08-18 12:48:06 -0700

    Fixed import paths on the tests

--- server/src/tests/personImageResolver.test.ts
+++ server/src/tests/personImageResolver.test.ts
@@ -1,10 +1,10 @@
 import { async } from "q";
 import { Connection } from "typeorm";
-import { gCall } from "../test-utils/gCall";
-import { registerOrLogin } from "../test-utils/registerOrLogin";
-import { contextSetup } from "../test-utils/setupGraphQLContext";
-import { testConn } from "../test-utils/testConn";
-import { IMyContext } from "../web/graphQL/context.interface";
+import { gCall } from "./test-utils/gCall";
+import { registerOrLogin } from "./test-utils/registerOrLogin";
+import { contextSetup } from "./test-utils/setupGraphQLContext";
+import { testConn } from "./test-utils/testConn";
+import { IMyContext } from "../graphQL/context.interface";
 
 let conn: Connection;
 let ctx: IMyContext;
