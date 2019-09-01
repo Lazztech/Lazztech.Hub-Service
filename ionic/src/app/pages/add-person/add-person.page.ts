90fae242a11d033fca02985bc2cb701edea90a2d
Gian Lazzarini <gianlazzarini@gmail.com>
2019-08-31 17:29:19 -0700

    Added camera to add-person page.

--- ionic/src/app/pages/add-person/add-person.page.ts
+++ ionic/src/app/pages/add-person/add-person.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-person',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPersonPage implements OnInit {
 
-  constructor() { }
+  photo: any;
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
+  }
+
+  async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
   }
 
 }
