83b5e6462b54636f76441f38a1fefe03b927d68c
Gian Lazzarini <gianlazzarini@gmail.com>
2019-09-11 21:19:34 -0700

    Fixed tslint bitwise warning for qr decoding.

--- server/src/graphQL/resolvers/hubResolver.ts
+++ server/src/graphQL/resolvers/hubResolver.ts
@@ -1,4 +1,4 @@
-import { BarcodeFormat, BinaryBitmap, DecodeHintType, HybridBinarizer, MultiFormatReader, RGBLuminanceSource } from '@zxing/library/esm5';
+import { BarcodeFormat, BinaryBitmap, DecodeHintType, HybridBinarizer, MultiFormatReader, RGBLuminanceSource } from "@zxing/library/esm5";
 import jpeg from "jpeg-js";
 import { verify } from "jsonwebtoken";
 import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
@@ -227,37 +227,38 @@ export class HubResolver {
     }
 
       public async scanQR(base64: string): Promise<any> {
-        const buff = Buffer.from(base64.substr(23), 'base64');
+        const buff = Buffer.from(base64.substr(23), "base64");
         console.log("created buff");
 
         const rawImageData = jpeg.decode(buff);
 
         const hints = new Map();
         const formats = [BarcodeFormat.QR_CODE, BarcodeFormat.DATA_MATRIX];
-         
+
         hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
         hints.set(DecodeHintType.TRY_HARDER, true);
-         
+
         const reader = new MultiFormatReader();
-         
+
         reader.setHints(hints);
-        
+
         const len = rawImageData.width * rawImageData.height;
-        
+
         const luminancesUint8Array = new Uint8ClampedArray(len);
-        
-        for(let i = 0; i < len; i++){
-            luminancesUint8Array[i] = ((rawImageData.data[i*4]+rawImageData.data[i*4+1]*2+rawImageData.data[i*4+2]) / 4) & 0xFF;
+
+        for (let i = 0; i < len; i++) {
+            // tslint:disable-next-line:no-bitwise
+            luminancesUint8Array[i] = ((rawImageData.data[i * 4] + rawImageData.data[i * 4 + 1] * 2 + rawImageData.data[i * 4 + 2]) / 4) & 0xFF;
         }
-        
+
         const luminanceSource = new RGBLuminanceSource(luminancesUint8Array, rawImageData.width, rawImageData.height);
-        
-        console.log(luminanceSource)
-        
+
+        console.log(luminanceSource);
+
         const binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
-        
+
         const qrCode = reader.decode(binaryBitmap);
-        
+
         console.log(qrCode);
 
         if (qrCode) {
