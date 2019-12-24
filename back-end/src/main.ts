948cd4af8b5af75e76d4b984fdb6e5d5b1f42cbb
Gian Lazzarini <gianlazzarini@gmail.com>
2019-12-24 13:30:51 -0800

    nest startup uses env.port || 8080

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
