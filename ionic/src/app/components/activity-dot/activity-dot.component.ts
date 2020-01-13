c40849b4a26ff0e24708613ac8a01a15f13f397b
Gian Lazzarini <gianlazzarini@gmail.com>
2020-01-12 16:16:08 -0800

    Working on activity dot component

---
+++ ionic/src/app/components/activity-dot/activity-dot.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-dot',
+  templateUrl: './activity-dot.component.html',
+  styleUrls: ['./activity-dot.component.scss'],
+})
+export class ActivityDotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- ionic/src/app/components/components.module.ts
+++ ionic/src/app/components/components.module.ts
@@ -3,11 +3,20 @@ import { GoogleMapComponent } from './google-map/google-map.component';
 import { HubCardComponent } from './hub-card/hub-card.component';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { ActivityDotComponent } from './activity-dot/activity-dot.component';
 
 @NgModule({
-    declarations: [GoogleMapComponent, HubCardComponent],
+    declarations: [
+        GoogleMapComponent, 
+        HubCardComponent,
+        ActivityDotComponent
+    ],
     imports: [IonicModule, CommonModule],
-    exports: [GoogleMapComponent, HubCardComponent]
+    exports: [
+        GoogleMapComponent, 
+        HubCardComponent,
+        ActivityDotComponent
+    ]
 })
 export class ComponentsModule {
 
