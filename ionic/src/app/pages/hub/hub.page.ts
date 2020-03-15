d450d3974c843eda286c3fe53424dad85aae12b9
Gian Lazzarini <gianlazzarini@gmail.com>
2020-03-15 13:10:48 -0700

    Added open nav in apple maps

--- ionic/src/app/pages/hub/hub.page.ts
+++ ionic/src/app/pages/hub/hub.page.ts
@@ -92,6 +92,15 @@ export class HubPage implements OnInit, OnDestroy {
     window.open(`uber://?client_id=<CLIENT_ID>&action=setPickup&pickup[latitude]=${this.userCoords.latitude}&pickup[longitude]=${this.userCoords.longitude}&pickup[nickname]=Your%20Location&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=${this.hubCoords.latitude}&dropoff[longitude]=${this.hubCoords.longitude}&dropoff[nickname]=${this.userHub.hub.name}%20Hub&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d&link_text=View%20team%20roster&partner_deeplink=partner%3A%2F%2Fteam%2F9383`)
   }
 
+  navigate() {
+    if (this.platform.is('ios')) {
+      window.open(`http://maps.apple.com/?saddr=${this.userCoords.latitude},${this.userCoords.longitude}&daddr=${this.hubCoords.latitude},${this.hubCoords.longitude}&dirflg=d`)
+    } 
+    else if (this.platform.is('android')) {
+      //TODO implement for android
+    }
+  }
+
   async presentActionSheet() {
     const editHubButton = (this.userHub.isOwner)
     ? {
