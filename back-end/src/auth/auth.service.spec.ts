d03b5a98f2d7d9dd82b050dcf9365fda3214ab5b
Gian Lazzarini <gianlazzarini@gmail.com>
2020-04-12 20:10:45 -0700

    Passed 'should return for register'

--- back-end/src/auth/auth.service.spec.ts
+++ back-end/src/auth/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { ConfigService } from '@nestjs/config';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { InAppNotification } from 'src/dal/entity/inAppNotification.entity';
@@ -8,15 +8,21 @@ import { Repository } from 'typeorm';
 import { AuthService } from './auth.service';
 import { NotificationService } from 'src/notification/notification.service';
 import { HttpModule } from '@nestjs/common';
+import configuration from 'src/config/configuration';
 
 describe('AuthService', () => {
   let service: AuthService;
   let userRepo: Repository<User>;
+  let notificationService: NotificationService;
   let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+          isGlobal: true,
+        }),
         HttpModule
       ],
       providers: [
@@ -40,6 +46,7 @@ describe('AuthService', () => {
 
     service = module.get<AuthService>(AuthService);
     userRepo = module.get<Repository<User>>(getRepositoryToken(User));
+    notificationService = module.get(NotificationService);
     configService = module.get(ConfigService);
   });
 
@@ -48,7 +55,26 @@ describe('AuthService', () => {
   });
 
   it('should return for register', async () => {
-    //TODO
+    //Arrange
+    const firstName = "Gian";
+    const lastName = "Lazzarini";
+    const email = "gianlazzarini@gmail.com";
+    const password = "Password123";
+    jest.spyOn(userRepo, 'findOne').mockResolvedValueOnce(undefined);
+    jest.spyOn(userRepo, 'create').mockReturnValueOnce(undefined);
+    const saveCall = jest.spyOn(userRepo, 'save').mockResolvedValueOnce({
+      firstName,
+      lastName,
+      email,
+    } as User);
+    jest.spyOn(notificationService, 'addInAppNotificationForUser').mockImplementationOnce(
+      () => Promise.resolve()
+    );
+    //Act
+    const result = await service.register(firstName, lastName, email, password);
+    //Assert
+    expect(result).toBeDefined();
+    expect(saveCall).toHaveBeenCalled();
   });
 
   it(`should return accessToken for login`, async () => {
