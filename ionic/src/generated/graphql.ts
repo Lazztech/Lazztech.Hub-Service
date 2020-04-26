09390b9cfc3ed4dfba2e8d7e3ed67203332dc9f6
Gian Lazzarini <gianlazzarini@gmail.com>
2020-04-26 13:52:06 -0700

    Setting up graphql-code-generator

---
+++ ionic/src/generated/graphql.ts
@@ -0,0 +1,295 @@
+import gql from 'graphql-tag';
+export type Maybe<T> = T | null;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+
+
+export type Hub = {
+   __typename?: 'Hub';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  active?: Maybe<Scalars['Boolean']>;
+  image?: Maybe<Scalars['String']>;
+  latitude?: Maybe<Scalars['Float']>;
+  longitude?: Maybe<Scalars['Float']>;
+  usersConnection?: Maybe<Array<JoinUserHub>>;
+  microChats?: Maybe<Array<MicroChat>>;
+};
+
+export type InAppNotification = {
+   __typename?: 'InAppNotification';
+  id: Scalars['ID'];
+  header?: Maybe<Scalars['String']>;
+  text: Scalars['String'];
+  date: Scalars['String'];
+  thumbnail?: Maybe<Scalars['String']>;
+  actionLink?: Maybe<Scalars['String']>;
+};
+
+export type Invite = {
+   __typename?: 'Invite';
+  id: Scalars['ID'];
+  email: Scalars['String'];
+};
+
+export type JoinUserHub = {
+   __typename?: 'JoinUserHub';
+  userId: Scalars['ID'];
+  hubId: Scalars['ID'];
+  user: User;
+  hub: Hub;
+  isOwner: Scalars['Boolean'];
+  starred: Scalars['Boolean'];
+  isPresent: Scalars['Boolean'];
+};
+
+export type JoinUserInAppNotifications = {
+   __typename?: 'JoinUserInAppNotifications';
+  userId: Scalars['ID'];
+  inAppNotificationId: Scalars['ID'];
+  user: User;
+  inAppNotification: InAppNotification;
+};
+
+export type MicroChat = {
+   __typename?: 'MicroChat';
+  id: Scalars['ID'];
+  hubId: Scalars['Float'];
+  hub: Scalars['ID'];
+  text: Scalars['String'];
+};
+
+export type Mutation = {
+   __typename?: 'Mutation';
+  createHub: Hub;
+  inviteUserToHub: Scalars['Boolean'];
+  deleteHub: Scalars['Boolean'];
+  editHub: Hub;
+  changeHubImage: Hub;
+  joinHub: Scalars['Boolean'];
+  setHubStarred: Scalars['Boolean'];
+  setHubNotStarred: Scalars['Boolean'];
+  enteredHubGeofence: Scalars['Boolean'];
+  exitedHubGeofence: Scalars['Boolean'];
+  activateHub: Hub;
+  deactivateHub: Hub;
+  microChatToHub: Scalars['Boolean'];
+  createMicroChat: MicroChat;
+  deleteMicroChat: Scalars['Boolean'];
+  editUserDetails: User;
+  changeEmail: User;
+  changeUserImage: User;
+  addUserFcmNotificationToken: Scalars['Boolean'];
+  deleteInAppNotification: Scalars['Boolean'];
+  deleteAllInAppNotifications: Scalars['Boolean'];
+  login?: Maybe<Scalars['String']>;
+  register?: Maybe<Scalars['String']>;
+  logout: Scalars['Boolean'];
+  resetPassword: Scalars['Boolean'];
+  sendPasswordResetEmail: Scalars['Boolean'];
+  changePassword: Scalars['Boolean'];
+  deleteAccount: Scalars['Boolean'];
+};
+
+
+export type MutationCreateHubArgs = {
+  longitude: Scalars['Float'];
+  latitude: Scalars['Float'];
+  image: Scalars['String'];
+  description: Scalars['String'];
+  name: Scalars['String'];
+};
+
+
+export type MutationInviteUserToHubArgs = {
+  inviteesEmail: Scalars['String'];
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationDeleteHubArgs = {
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationEditHubArgs = {
+  description: Scalars['String'];
+  name: Scalars['String'];
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationChangeHubImageArgs = {
+  newImage: Scalars['String'];
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationJoinHubArgs = {
+  id: Scalars['Int'];
+};
+
+
+export type MutationSetHubStarredArgs = {
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationSetHubNotStarredArgs = {
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationEnteredHubGeofenceArgs = {
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationExitedHubGeofenceArgs = {
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationActivateHubArgs = {
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationDeactivateHubArgs = {
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationMicroChatToHubArgs = {
+  microChatId: Scalars['Int'];
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationCreateMicroChatArgs = {
+  microChatText: Scalars['String'];
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationDeleteMicroChatArgs = {
+  microChatId: Scalars['Int'];
+  hubId: Scalars['Int'];
+};
+
+
+export type MutationEditUserDetailsArgs = {
+  description: Scalars['String'];
+  lastName: Scalars['String'];
+  firstName: Scalars['String'];
+};
+
+
+export type MutationChangeEmailArgs = {
+  newEmail: Scalars['String'];
+};
+
+
+export type MutationChangeUserImageArgs = {
+  newImage: Scalars['String'];
+};
+
+
+export type MutationAddUserFcmNotificationTokenArgs = {
+  token: Scalars['String'];
+};
+
+
+export type MutationDeleteInAppNotificationArgs = {
+  inAppNotificationId: Scalars['Int'];
+};
+
+
+export type MutationLoginArgs = {
+  password: Scalars['String'];
+  email: Scalars['String'];
+};
+
+
+export type MutationRegisterArgs = {
+  data: UserInput;
+};
+
+
+export type MutationResetPasswordArgs = {
+  newPassword: Scalars['String'];
+  resetPin: Scalars['String'];
+  usersEmail: Scalars['String'];
+};
+
+
+export type MutationSendPasswordResetEmailArgs = {
+  email: Scalars['String'];
+};
+
+
+export type MutationChangePasswordArgs = {
+  newPassword: Scalars['String'];
+  oldPassword: Scalars['String'];
+};
+
+
+export type MutationDeleteAccountArgs = {
+  password: Scalars['String'];
+  email: Scalars['String'];
+};
+
+export type Query = {
+   __typename?: 'Query';
+  hub: JoinUserHub;
+  usersHubs: Array<JoinUserHub>;
+  commonUsersHubs: Array<JoinUserHub>;
+  usersPeople: Array<User>;
+  searchHubByName: Array<Hub>;
+  ownedHubs: Array<Hub>;
+  memberOfHubs: Array<Hub>;
+  me?: Maybe<User>;
+  getInAppNotifications: Array<InAppNotification>;
+};
+
+
+export type QueryHubArgs = {
+  id: Scalars['Int'];
+};
+
+
+export type QueryCommonUsersHubsArgs = {
+  otherUsersId: Scalars['Int'];
+};
+
+
+export type QuerySearchHubByNameArgs = {
+  search: Scalars['String'];
+};
+
+export type User = {
+   __typename?: 'User';
+  id: Scalars['ID'];
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  image?: Maybe<Scalars['String']>;
+  email: Scalars['String'];
+};
+
+export type UserInput = {
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+
