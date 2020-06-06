0a9c5d6ebc9b6269df348b24fd422594d3a50ffe
Gian Lazzarini <gianlazzarini@gmail.com>
2020-06-06 14:04:52 -0700

    ng add apollo-angular

--- ionic/src/app/app.module.ts
+++ ionic/src/app/app.module.ts
@@ -9,14 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { ApolloModule, Apollo } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { SERVER_URL } from 'src/environments/environment';
 
-import { setContext } from "apollo-link-context";
-import { Storage, IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
@@ -29,8 +24,9 @@ import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 import { SentryIonicErrorHandler } from './errors/sentryIonicErrorHandler';
 import * as Sentry from "@sentry/browser";
 import { HttpRequestInterceptor } from './interceptors/http.interceptor';
-import { LoggerModule, NGXLogger } from 'ngx-logger';
+import { LoggerModule } from 'ngx-logger';
 import { Diagnostic } from '@ionic-native/diagnostic/ngx';
+import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -41,12 +37,11 @@ import { Diagnostic } from '@ionic-native/diagnostic/ngx';
     AppRoutingModule,
     // HttpClientModule,
     NativeHttpModule,
-    ApolloModule,
-    HttpLinkModule,
     IonicStorageModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ReactiveFormsModule,
     LoggerModule.forRoot(environment.logging),
+    GraphQLModule,
   ],
   providers: [
     StatusBar,
@@ -63,39 +58,8 @@ import { Diagnostic } from '@ionic-native/diagnostic/ngx';
 })
 export class AppModule {
   constructor(
-    apollo: Apollo, 
-    httpLink: HttpLink, 
-    storage: Storage,
-    private logger: NGXLogger
     ) {
-
     Sentry.init(environment.sentry);
-
-    const apolloLink = httpLink.create({
-      uri: SERVER_URL,
-      withCredentials: true
-    });
-
-    const auth = setContext(async (_, { headers }) => {
-      const token = await storage.get('token');
-      if (!token) {
-        this.logger.error("Couldn't add jwt to header.");
-        return {};
-      } else {
-        return {
-          headers: {
-            ...headers,
-            Authorization: token
-          }
-        };
-      }
-    });
-
-
-    apollo.create({
-      link: auth.concat(apolloLink),
-      cache: new InMemoryCache(),
-    })
   }
 }
 
---
+++ ionic/src/app/graphql.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { NGXLogger } from 'ngx-logger';
+import { SERVER_URL } from 'src/environments/environment';
+
+
+export function createApollo(
+  httpLink: HttpLink,
+  storage: Storage,
+  logger: NGXLogger
+) {
+
+  const apolloLink = httpLink.create({
+    uri: SERVER_URL,
+    withCredentials: true
+  });
+
+  const auth = setContext(async (_, { headers }) => {
+    const token = await storage.get('token');
+    if (!token) {
+      logger.error("Couldn't add jwt to header.");
+      return {};
+    } else {
+      return {
+        headers: {
+          ...headers,
+          Authorization: token
+        }
+      };
+    }
+  });
+
+  return {
+    link: auth.concat(apolloLink),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  exports: [ApolloModule, HttpLinkModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink, Storage, NGXLogger],
+    },
+  ],
+})
+export class GraphQLModule { }
