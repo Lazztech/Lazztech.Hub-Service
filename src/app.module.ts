9c644d4819b93fd54d723b4e178313eb4a6facdb
Gian Lazzarini <gianlazzarini@gmail.com>
2020-12-13 17:49:57 -0800

    Setup nestjs-admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,15 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { HealthController } from './health/health.controller';
 import { S3Module, S3ModuleOptions } from 'nestjs-s3';
+import { DefaultAdminModule, DefaultAdminSite } from 'nestjs-admin';
+import { Hub } from './dal/entity/hub.entity';
+import { InAppNotification } from './dal/entity/inAppNotification.entity';
+import { Invite } from './dal/entity/invite.entity';
+import { JoinUserHub } from './dal/entity/joinUserHub.entity';
+import { JoinUserInAppNotifications } from './dal/entity/joinUserInAppNotifications.entity';
+import { MicroChat } from './dal/entity/microChat.entity';
+import { PasswordReset } from './dal/entity/passwordReset.entity';
+import { UserDevice } from './dal/entity/userDevice.entity';
 
 @Module({
   imports: [
@@ -18,6 +27,7 @@ import { S3Module, S3ModuleOptions } from 'nestjs-s3';
       envFilePath: ['.env.local', '.env'],
       isGlobal: true,
     }),
+    DefaultAdminModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -34,7 +44,10 @@ import { S3Module, S3ModuleOptions } from 'nestjs-s3';
         logging: true,
         // migrationsRun: true,
         synchronize: true,
-        entities: [__dirname + '/dal/entity/**/*.*.*'],
+        entities: [
+          __dirname + '/dal/entity/**/*.*.*',
+          'node_modules/nestjs-admin/**/*.entity.js'
+        ],
         migrations: [__dirname + '/dal/migrations/**/*.*'],
         subscribers: [__dirname + '/dal/migrations/**/*.*'],
       }),
@@ -71,4 +84,17 @@ import { S3Module, S3ModuleOptions } from 'nestjs-s3';
   controllers: [HealthController],
   providers: [AuthGuard],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly adminSite: DefaultAdminSite) {
+    // Register the User entity under the "User" section
+    adminSite.register('Lazztech Hub', Hub);
+    adminSite.register('Lazztech Hub', InAppNotification);
+    adminSite.register('Lazztech Hub', Invite); 
+    adminSite.register('Lazztech Hub', JoinUserHub);
+    adminSite.register('Lazztech Hub', JoinUserInAppNotifications);
+    adminSite.register('Lazztech Hub', MicroChat);
+    adminSite.register('Lazztech Hub', PasswordReset);
+    adminSite.register('Lazztech Hub', User);
+    adminSite.register('Lazztech Hub', UserDevice);
+  }
+}
