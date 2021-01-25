fba4546cb2bee870858aee93c6cae1e73198e0be
Gian Lazzarini <gianlazzarini@gmail.com>
2021-01-24 18:00:44 -0800

    fixing UserId decorator

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,11 +1,10 @@
-import { createParamDecorator, Logger } from '@nestjs/common';
-import { verify } from 'jsonwebtoken';
+import { createParamDecorator, ExecutionContext, Logger } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
-export const UserId = createParamDecorator((data, [root, args, ctx, info]) => {
-  const logger = new Logger(createParamDecorator.name, true);
-  logger.log('executing');
-
-  const userId = ctx.req.headers.userId;
-  // let accessToken = ctx.getContext().req.headers['authorization'];
-  return userId;
-});
+export const UserId = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const logger = new Logger(createParamDecorator.name, true);
+    logger.log('executing');
+    return GqlExecutionContext.create(ctx).getContext().req.headers.userId;
+  },
+);
