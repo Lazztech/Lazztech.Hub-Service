9becb82331ffb2293f6a8981cc307186ecc093a8
Gian Lazzarini <gianlazzarini@gmail.com>
2021-05-31 18:40:41 -0700

    1: Intial sqlite support setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
@@ -11,6 +11,8 @@ import { AuthModule } from './auth/auth.module';
 import { HealthController } from './health/health.controller';
 import { S3Module, S3ModuleOptions } from 'nestjs-s3';
 import { FieldResolversModule } from './dal/field-resolvers/field-resolvers.module';
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 @Module({
   imports: [
@@ -21,24 +23,46 @@ import { FieldResolversModule } from './dal/field-resolvers/field-resolvers.modu
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres' as const,
-        host: configService.get('DATABASE_HOST', 'localhost'),
-        port: configService.get<number>('DATABASE_PORT', 5432),
-        username: configService.get('DATABASE_USER', 'postgres'),
-        password: configService.get('DATABASE_PASS', 'postgres'),
-        database: configService.get('DATABASE_SCHEMA', 'postgres'),
-        extra: {
-          ssl:
-            configService.get('DATABASE_SSL', false) === 'true' ? true : false,
-        },
-        logging: true,
-        // migrationsRun: true,
-        synchronize: true,
-        entities: [__dirname + '/dal/entity/**/*.*.*'],
-        migrations: [__dirname + '/dal/migrations/**/*.*'],
-        subscribers: [__dirname + '/dal/migrations/**/*.*'],
-      }),
+      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
+        const commonSettings = {
+          logging: true,
+          // migrationsRun: true,
+          synchronize: true,
+          entities: [__dirname + '/dal/entity/**/*.*.*'],
+          migrations: [__dirname + '/dal/migrations/**/*.*'],
+          subscribers: [__dirname + '/dal/migrations/**/*.*'],
+        };
+        AppModule.logger.log(`DATABASE_TYPE: ${configService.get<string>('DATABASE_TYPE', 'sqlite')}`);
+        AppModule.logger.log(`DATABASE_SCHEMA: ${configService.get<string>('DATABASE_SCHEMA', `${__dirname}/../hub-service.db`)}`);
+        switch (configService.get('DATABASE_TYPE', 'sqlite')) {
+          case 'sqlite':
+            return {
+              ...commonSettings,
+              type: 'sqlite',
+              database: configService.get('DATABASE_SCHEMA', `${__dirname}/../hub-service.db`),
+            } as SqliteConnectionOptions;
+          case 'postgres':
+            return {
+              ...commonSettings,
+              type: 'postgres' as const,
+              database: configService.get('DATABASE_SCHEMA', 'postgres'),
+              host: configService.get('DATABASE_HOST', 'localhost'),
+              port: configService.get<number>('DATABASE_PORT', 5432),
+              username: configService.get('DATABASE_USER', 'postgres'),
+              password: configService.get('DATABASE_PASS', 'postgres'),
+              extra: {
+                ssl:
+                  configService.get('DATABASE_SSL', false) === 'true'
+                    ? true
+                    : false,
+              },
+            } as PostgresConnectionOptions;
+          default:
+            throw new Error(
+              'Invalid database type selected. It must be either sqlite (default) or postgres.',
+            );
+        }
+      },
     } as TypeOrmModuleOptions),
     TypeOrmModule.forFeature([User]),
     S3Module.forRootAsync({
@@ -81,4 +105,6 @@ import { FieldResolversModule } from './dal/field-resolvers/field-resolvers.modu
   ],
   controllers: [HealthController],
 })
-export class AppModule {}
+export class AppModule {
+  public static logger = new Logger(AppModule.name, true);
+}
