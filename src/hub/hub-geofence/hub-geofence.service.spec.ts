90fbe21eb465614013171b34a64067fb89abbb45
Gian Lazzarini <gianlazzarini@gmail.com>
2021-06-20 17:45:51 -0700

    13: updating hub-geofence tests

--- src/hub/hub-geofence/hub-geofence.service.spec.ts
+++ src/hub/hub-geofence/hub-geofence.service.spec.ts
@@ -6,13 +6,19 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../../dal/entity/user.entity';
 import { Hub } from '../../dal/entity/hub.entity';
 import { NotificationService } from '../../notification/notification.service';
+import { ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/common';
+import { InAppNotification } from '../../dal/entity/inAppNotification.entity';
+import { UserDevice } from '../../dal/entity/userDevice.entity';
 
 describe('HubGeofenceService', () => {
   let service: HubGeofenceService;
   let joinUserHubRepository: Repository<JoinUserHub>;
+  let hubRepository: Repository<Hub>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [
         HubGeofenceService,
         {
@@ -27,6 +33,29 @@ describe('HubGeofenceService', () => {
           provide: getRepositoryToken(Hub),
           useClass: Repository,
         },
+        {
+          provide: getRepositoryToken(InAppNotification),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(UserDevice),
+          useClass: Repository,
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              switch (key) {
+                case 'FIREBASE_SERVER_KEY':
+                  return 'mockFirebaseServerKey';
+                case 'PUSH_NOTIFICATION_ENDPOINT':
+                  return 'http://localhost/mockUrl';
+                default:
+                  return '';
+              }
+            }),
+          },
+        },
         NotificationService,
       ],
     }).compile();
@@ -35,6 +64,7 @@ describe('HubGeofenceService', () => {
     joinUserHubRepository = module.get<Repository<JoinUserHub>>(
       getRepositoryToken(JoinUserHub),
     );
+    hubRepository = module.get<Repository<Hub>>(getRepositoryToken(Hub));
   });
 
   it('should be defined', () => {
@@ -48,10 +78,18 @@ describe('HubGeofenceService', () => {
     const hubRelationshipTest = {
       userId,
       hubId,
+      hub: Promise.resolve({
+        active: true,
+      } as Hub),
     } as JoinUserHub;
     jest
       .spyOn(joinUserHubRepository, 'findOne')
       .mockResolvedValueOnce(hubRelationshipTest);
+
+    const notifyMembersSpy = jest
+      .spyOn(service, 'notifyMembersOfArrival')
+      .mockResolvedValue();
+
     const saveCall = jest
       .spyOn(joinUserHubRepository, 'save')
       .mockResolvedValueOnce({
@@ -63,6 +101,7 @@ describe('HubGeofenceService', () => {
     await service.enteredHubGeofence(userId, hubId);
     // Assert
     expect(saveCall).toHaveBeenCalled();
+    expect(notifyMembersSpy).toHaveBeenCalled();
   });
 
   it('should return for exitedHubGeofence', async () => {
@@ -75,16 +114,25 @@ describe('HubGeofenceService', () => {
     jest
       .spyOn(joinUserHubRepository, 'findOne')
       .mockResolvedValueOnce(hubRelationshipTest);
+
+    const notifyMembersSpy = jest
+      .spyOn(service, 'notifyMembersOfExit')
+      .mockResolvedValue();
+
     const saveCall = jest
       .spyOn(joinUserHubRepository, 'save')
       .mockResolvedValueOnce({
         userId,
         hubId,
         isOwner: false,
+        hub: Promise.resolve({
+          active: true,
+        } as Hub),
       } as JoinUserHub);
     // Act
     await service.exitedHubGeofence(userId, hubId);
     // Assert
     expect(saveCall).toHaveBeenCalled();
+    expect(notifyMembersSpy).toHaveBeenCalled();
   });
 });
