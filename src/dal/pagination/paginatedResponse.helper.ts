957a092ebcf661d1a03185269b2c1d6e67359d3f
Gian Lazzarini <gianlazzarini@gmail.com>
2021-11-18 17:30:40 -0800

    Notifications pagination (#20)
    * new paginatedInAppNotifications query
    
    * added sortOptions to PageableOptions
    
    * lint & format
    
    * docker buildx for m1 to x86 builds

---
+++ src/dal/pagination/paginatedResponse.helper.ts
@@ -0,0 +1,64 @@
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import { InAppNotification } from '../entity/inAppNotification.entity';
+
+export interface ClassType<T = any> {
+  new (...args: any[]): T;
+}
+
+export default function PaginatedResponse<TItem>(TItemClass: ClassType<TItem>) {
+  // `isAbstract` decorator option is mandatory to prevent registering in schema
+  @ObjectType({ isAbstract: true })
+  abstract class PaginatedResponseClass {
+    // here we use the runtime argument
+    @Field(() => [TItemClass])
+    // and here the generic type
+    items: TItem[];
+
+    @Field(() => Int)
+    total: number;
+  }
+  return PaginatedResponseClass;
+}
+
+@ObjectType()
+export class PaginatedInAppNotificationsResponse extends PaginatedResponse(
+  InAppNotification,
+) {}
+
+@InputType()
+export class SortOptions {
+  @Field()
+  public field: string;
+
+  @Field()
+  public ascending: boolean;
+}
+
+@InputType()
+export class PageableOptions {
+  @Field(() => Int, { nullable: true })
+  public limit?: number;
+
+  @Field(() => Int, { nullable: true })
+  public offset?: number;
+
+  @Field({ nullable: true })
+  public sortOptions?: SortOptions;
+}
+
+/**
+ *
+ * @param sortOptions
+ * @returns typeorm order param compatible result
+ */
+export function generateTypeOrmOrderOptions(
+  sortOptions?: SortOptions,
+): {
+  [field: string]: 'ASC' | 'DESC';
+} {
+  return sortOptions
+    ? {
+        [`${sortOptions?.field}`]: sortOptions?.ascending ? 'ASC' : 'DESC',
+      }
+    : undefined;
+}
--- src/notification/notification.resolver.ts
+++ src/notification/notification.resolver.ts
@@ -1,9 +1,20 @@
 import { Logger, UseGuards } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver, ID, Int } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Query,
+  Resolver,
+  ID,
+  Directive,
+} from '@nestjs/graphql';
 import { UserId } from '../decorators/user.decorator';
 import { GqlJwtAuthGuard } from '../auth/guards/gql-jwt-auth.guard';
 import { InAppNotification } from '../dal/entity/inAppNotification.entity';
 import { NotificationService } from './notification.service';
+import {
+  PageableOptions,
+  PaginatedInAppNotificationsResponse,
+} from '../dal/pagination/paginatedResponse.helper';
 
 @UseGuards(GqlJwtAuthGuard)
 @Resolver()
@@ -24,18 +35,35 @@ export class NotificationResolver {
     return true;
   }
 
+  @Directive(
+    '@deprecated(reason: "This query has been replaced with a new paginated version")',
+  )
   @Query(() => [InAppNotification])
   public async getInAppNotifications(
     @UserId() userId,
-    @Args({ name: 'limit', type: () => Int, nullable: true }) limit: number,
-    @Args({ name: 'offset', type: () => Int, nullable: true }) offset: number,
   ): Promise<InAppNotification[]> {
     this.logger.log(this.getInAppNotifications.name);
-    return await this.notificationService.getInAppNotifications(
+    const [result] = await this.notificationService.getInAppNotifications(
       userId,
-      limit,
-      offset,
     );
+    return result;
+  }
+
+  @Query(() => PaginatedInAppNotificationsResponse)
+  public async paginatedInAppNotifications(
+    @UserId() userId,
+    @Args('pageableOptions', { nullable: true })
+    pageableOptions?: PageableOptions,
+  ): Promise<PaginatedInAppNotificationsResponse> {
+    this.logger.log(this.getInAppNotifications.name);
+    const [items, total] = await this.notificationService.getInAppNotifications(
+      userId,
+      pageableOptions,
+    );
+    return {
+      items,
+      total,
+    };
   }
 
   @Mutation(() => Boolean)
