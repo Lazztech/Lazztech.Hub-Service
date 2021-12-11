fd625e898d6f8eb6ad2862d43287d904090395a1
leolazz <leolazzarini101@gmail.com>
2021-12-10 17:11:04 -0800

    migrations working on postgres and sqlite. scripts for each added.

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,34 +1,33 @@
-const path = require("path");
-
 const commonSettings = {
   migrationsRun: true,
   synchronize: false,
   entities: ['dist/src/dal/entity/**/*.*.*'],
-  // migrations: [__dirname + '/dal/migrations/**/*.*'],
-  migrations: ['dist/src/dal/migrations/*.js'],
   subscribers: [__dirname + '/dal/migrations/**/*.*'],
-  cli: {
-    migrationsDir: 'src/dal/migrations',
-  }
 };
-// Check typeORM documentation for more information.
+
 const config = [{
   name: 'prod',
   type: 'postgres',
   host: 'localhost',
   database: 'postgres',
   port: 5432,
-  // schema: 'postgres',
   username: 'postgres',
   password: 'Password123',
   ssl: false,
+  migrations: ['dist/src/dal/migrations/postgres/*.js'],
+  cli: {
+    migrationsDir: 'src/dal/migrations/postgres/',
+  },
   ...commonSettings
 },{
   name: 'default',
   type: 'sqlite',
   database: 'data/sqlite3.db',
+  migrations: ['dist/src/dal/migrations/sqlite/*.js'],
+  cli: {
+    migrationsDir: 'src/dal/migrations/sqlite',
+  },
   ...commonSettings
 },];
-
 
 module.exports = config
