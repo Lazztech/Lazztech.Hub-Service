360f03e939090a68879de78ed7fd0d58357de9f2
Gian Lazzarini <gianlazzarini@gmail.com>
2022-01-10 22:26:39 -0800

    cleaned up configs

--- mikro-orm.sqlite.config.js
+++ mikro-orm.sqlite.config.js
@@ -1,36 +1,3 @@
-// const commonSettings = {
-//   migrationsRun: false,
-//   synchronize: false,
-//   entities: ['src/dal/entity/**/*.*.*'],
-// };
-
-// const config = [{
-//   name: 'postgres',
-//   type: 'postgres',
-//   host: 'localhost',
-//   database: 'postgres',
-//   port: 5432,
-//   username: 'postgres',
-//   password: 'Password123',
-//   ssl: false,
-//   migrations: ['src/dal/migrations/postgres/*.ts'],
-//   cli: {
-//     migrationsDir: 'src/dal/migrations/postgres',
-//   },
-//   ...commonSettings
-// },{
-//   name: 'default',
-//   type: 'sqlite',
-//   database: __dirname + '/data/sqlite3.db',
-//   migrations: ['src/dal/migrations/sqlite/*.ts'],
-//   cli: {
-//     migrationsDir: 'src/dal/migrations/sqlite',
-//   },
-//   ...commonSettings
-// },];
-
-// module.exports = config
-
 const mikroOrmSqliteConfig = {
   type: 'sqlite',
   contextName: 'sqliteConfig',
