0379c6e2a5d3a19fb535e5c641e81ae8470a1996
Gian Lazzarini <gianlazzarini@gmail.com>
2022-01-10 22:25:34 -0800

    prepared mikro-orm sqlite & postgres configs, and scripts

---
+++ mikro-orm.postgres.config.js
@@ -0,0 +1,50 @@
+// const commonSettings = {
+//   migrationsRun: false,
+//   synchronize: false,
+//   entities: ['src/dal/entity/**/*.*.*'],
+// };
+
+// const config = [{
+//   name: 'postgres',
+//   type: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'Password123',
+//   ssl: false,
+//   migrations: ['src/dal/migrations/postgres/*.ts'],
+//   cli: {
+//     migrationsDir: 'src/dal/migrations/postgres',
+//   },
+//   ...commonSettings
+// },{
+//   name: 'default',
+//   type: 'sqlite',
+//   database: __dirname + '/data/sqlite3.db',
+//   migrations: ['src/dal/migrations/sqlite/*.ts'],
+//   cli: {
+//     migrationsDir: 'src/dal/migrations/sqlite',
+//   },
+//   ...commonSettings
+// },];
+
+// module.exports = config
+
+const mikroOrmPostgresConfig = {
+  type: 'postgresql',
+  contextName: 'postgreConfig',
+  name: 'postgre',
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'Password123',
+  dbName: 'postgres',
+  entities: ['src/dal/entity/**/*.*.*'],
+  migrations: {
+    path: 'src/dal/migrations/postgres/*.ts',
+  },
+  debug: true,
+};
+
+module.exports = mikroOrmPostgresConfig;
