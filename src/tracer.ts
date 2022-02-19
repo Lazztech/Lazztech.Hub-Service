94b526f190197961dbf4117a75bca9db040ef9a1
Gian Lazzarini <gianlazzarini@gmail.com>
2022-02-19 14:09:37 -0800

    wip

--- src/tracer.ts
+++ src/tracer.ts
@@ -4,14 +4,14 @@
 
 const opentelemetry = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-grpc');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
 // configure the SDK to export telemetry data to the console
 // enable all auto-instrumentations from the meta package
 const exporterOptions = {
-  url: 'http://<IP of SigNoz>:4317/v1/trace',
+  url: 'http://localhost:4317/v1/trace',
  }
 const traceExporter = new OTLPTraceExporter(exporterOptions);
 const sdk = new opentelemetry.NodeSDK({
