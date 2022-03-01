c57faf443c62ecc835d3ab9d32ca56e36b4255d4
Gian Lazzarini <gianlazzarini@gmail.com>
2022-02-28 19:04:11 -0800

    addded missing invite field resolvers (#30)

--- src/dal/field-resolvers/invite-field.resolver.ts
+++ src/dal/field-resolvers/invite-field.resolver.ts
@@ -1,5 +1,7 @@
 import { ID, Parent, ResolveField, Resolver } from "@nestjs/graphql";
+import { Hub } from "../entity/hub.entity";
 import { Invite } from "../entity/invite.entity";
+import { User } from "../entity/user.entity";
 
 @Resolver((of) => Invite)
 export class InviteFieldResolver {
@@ -17,6 +19,21 @@ export class InviteFieldResolver {
     @ResolveField(() => ID)
     hubId(@Parent() parent: Invite) {
       return parent.hub.id;
+    }
+
+    @ResolveField(() => User)
+    async inviter(@Parent() parent: Invite) {
+        return parent.inviter.load();
+    }
+
+    @ResolveField(() => User)
+    async invitee(@Parent() parent: Invite) {
+        return parent.invitee.load();
+    }
+
+    @ResolveField(() => Hub)
+    async hub(@Parent() parent: Invite) {
+        return parent.hub.load();
     }
     
 }
