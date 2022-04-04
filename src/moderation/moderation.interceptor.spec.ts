49e60ec7e8fa25d9f83417c88def6d380b00aba2
Gian Lazzarini <gianlazzarini@gmail.com>
2022-04-03 19:13:53 -0700

    moderation interceptor tests

--- src/moderation/moderation.interceptor.spec.ts
+++ src/moderation/moderation.interceptor.spec.ts
@@ -1,7 +1,61 @@
+import { of } from 'rxjs';
 import { ModerationInterceptor } from './moderation.interceptor';
 
 describe('ModerationInterceptor', () => {
+  const interceptor = new ModerationInterceptor();
+
   it('should be defined', () => {
-    expect(new ModerationInterceptor()).toBeDefined();
+    expect(interceptor).toBeDefined();
   });
+
+  it('should not intercept objects unless they have truthy banned field', async () => {
+    // arrange
+    const mockNonBannedResponse = {
+      id: 'notBanned'
+    };
+    const mockCallHandler = {
+      handle: () => of(mockNonBannedResponse)
+    };
+    // act
+    const result = await interceptor.intercept(null, mockCallHandler).toPromise();
+    // assert
+    expect(result).toBe(mockNonBannedResponse);
+  });
+
+  it('should intercept banned objects', async () => {
+    // arrange
+    const mockBannedResponse = {
+      banned: true
+    };
+    const mockCallHandler = {
+      handle: () => of(mockBannedResponse)
+    };
+    // act
+    const result = await interceptor.intercept(null, mockCallHandler).toPromise();
+    // assert
+    expect(result).toBeUndefined();
+  });
+
+  it('should intercept banned objects from an array', async () => {
+    // arrange
+    const mockNonBannedResponse = {
+      id: 'notBanned'
+    };
+    const mockBannedResponse = {
+      banned: true
+    };
+    const mockCallHandler = {
+      handle: () => of([
+        mockNonBannedResponse,
+        mockBannedResponse
+      ])
+    };
+
+    // act
+    const result = await interceptor.intercept(null, mockCallHandler).toPromise();
+
+    // assert
+    expect(result).toEqual([mockNonBannedResponse]);
+  });
+
 });
