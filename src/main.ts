614e29368e3268808630bd6b4d8b469802854f18
Gian Lazzarini <gianlazzarini@gmail.com>
2022-10-25 21:39:51 -0700

    removed sentry logging as every log seems to result in an issue on sentry?

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  app.useLogger(SentryService.SentryServiceInstance());
   app.useGlobalInterceptors(new ModerationInterceptor());
   await app.listen(process.env.PORT || 8080);
 }
