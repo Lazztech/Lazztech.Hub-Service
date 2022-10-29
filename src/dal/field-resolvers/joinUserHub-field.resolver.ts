83684ae818c81853e0906941b53979ab072c8589
Gian Lazzarini <gianlazzarini@gmail.com>
2022-10-28 18:54:02 -0700

    field resolver cleanup of unused repository

--- src/dal/field-resolvers/joinUserHub-field.resolver.ts
+++ src/dal/field-resolvers/joinUserHub-field.resolver.ts
@@ -8,12 +8,6 @@ import { User } from '../entity/user.entity';
 
 @Resolver(() => JoinUserHub)
 export class JoinUserHubsResolver {
-  private logger = new Logger(JoinUserHubsResolver.name);
-
-  constructor(
-    @InjectRepository(JoinUserHub)
-    private joinUserHubRepository: EntityRepository<JoinUserHub>,
-  ) {}
 
   @ResolveField(() => ID)
   public userId(@Parent() joinUserHub: JoinUserHub) {
