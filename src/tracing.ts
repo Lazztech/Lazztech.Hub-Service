4b2a36ce1436c853af21d02e551009cfc820401a
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-02 18:54:54 -0700

    formatted tracing.ts

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,39 +1,39 @@
 import {
-    CompositePropagator,
-    W3CTraceContextPropagator,
-    W3CBaggagePropagator,
-  } from '@opentelemetry/core';
-  import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-  import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
-  import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-  import { JaegerPropagator } from '@opentelemetry/propagator-jaeger';
-  import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3';
-  import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
-  import { NodeSDK } from '@opentelemetry/sdk-node';
-  import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
+  CompositePropagator,
+  W3CTraceContextPropagator,
+  W3CBaggagePropagator,
+} from '@opentelemetry/core';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { JaegerPropagator } from '@opentelemetry/propagator-jaeger';
+import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
 import { PinoInstrumentation } from '@opentelemetry/instrumentation-pino';
-  
-  const otelSDK = new NodeSDK({
-    metricReader: new PrometheusExporter({
-      port: 8081,
-    }),
-    spanProcessor: new BatchSpanProcessor(new JaegerExporter()),
-    contextManager: new AsyncLocalStorageContextManager(),
-    textMapPropagator: new CompositePropagator({
-      propagators: [
-        new JaegerPropagator(),
-        new W3CTraceContextPropagator(),
-        new W3CBaggagePropagator(),
-        new B3Propagator(),
-        new B3Propagator({
-          injectEncoding: B3InjectEncoding.MULTI_HEADER,
-        }),
-      ],
-    }),
-    instrumentations: [
-      // getNodeAutoInstrumentations(),
-      new PinoInstrumentation(),
+
+const otelSDK = new NodeSDK({
+  metricReader: new PrometheusExporter({
+    port: 8081,
+  }),
+  spanProcessor: new BatchSpanProcessor(new JaegerExporter()),
+  contextManager: new AsyncLocalStorageContextManager(),
+  textMapPropagator: new CompositePropagator({
+    propagators: [
+      new JaegerPropagator(),
+      new W3CTraceContextPropagator(),
+      new W3CBaggagePropagator(),
+      new B3Propagator(),
+      new B3Propagator({
+        injectEncoding: B3InjectEncoding.MULTI_HEADER,
+      }),
     ],
-  });
-  
-  export default otelSDK;
+  }),
+  instrumentations: [
+    // getNodeAutoInstrumentations(),
+    new PinoInstrumentation(),
+  ],
+});
+
+export default otelSDK;
