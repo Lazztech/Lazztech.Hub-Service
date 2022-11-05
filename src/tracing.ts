fa26732dc38bbcb9ab24f0022c838ace248187e6
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-04 18:34:21 -0700

    signoz it is for now

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,35 +1,15 @@
-import {
-  CompositePropagator,
-  W3CTraceContextPropagator,
-  W3CBaggagePropagator,
-} from '@opentelemetry/core';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { JaegerPropagator } from '@opentelemetry/propagator-jaeger';
-import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3';
-import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { Resource } from '@opentelemetry/resources';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
 const otelSDK = new NodeSDK({
-  metricReader: new PrometheusExporter({
-    port: 8081,
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://143.244.157.167:4318/v1/traces'
   }),
-  spanProcessor: new BatchSpanProcessor(new JaegerExporter()),
-  traceExporter: new OTLPTraceExporter({}),
-  contextManager: new AsyncLocalStorageContextManager(),
-  textMapPropagator: new CompositePropagator({
-    propagators: [
-      new JaegerPropagator(),
-      new W3CTraceContextPropagator(),
-      new W3CBaggagePropagator(),
-      new B3Propagator(),
-      new B3Propagator({
-        injectEncoding: B3InjectEncoding.MULTI_HEADER,
-      }),
-    ],
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'Lazztech Hub',
   }),
   instrumentations: [
     getNodeAutoInstrumentations({
