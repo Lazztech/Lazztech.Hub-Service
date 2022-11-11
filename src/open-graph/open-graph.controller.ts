9bf8cd270d38c9aaa1b6dada3d4ce92a1d77f3d8
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-10 22:23:02 -0800

    WIP open-graph controller

--- src/open-graph/open-graph.controller.ts
+++ src/open-graph/open-graph.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param} from '@nestjs/common';
 import { OpenGraphService } from './open-graph.service';
 
 @Controller('open-graph')
 export class OpenGraphController {
-  constructor(private readonly openGraphService: OpenGraphService) {}
+
+  constructor(
+    private readonly openGraphService: OpenGraphService
+  ) {}
+
+  @Get('/title/:shareableId')
+  getTitle(@Param('shareableId') shareableId: string) {
+    return shareableId;
+  }
+
+  @Get('/description/:shareableId')
+  getDescription(@Param('shareableId') shareableId: string) {
+    return shareableId;
+  }
+
+  @Get('/image/:shareableId')
+  getImage(@Param('shareableId') shareableId: string) {
+    return shareableId;
+  }
+
 }
