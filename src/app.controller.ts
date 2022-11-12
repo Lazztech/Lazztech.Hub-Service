80bb56d42b08a51cc8e94e3f3a3481ce075de9b3
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-11 19:56:27 -0800

    deleted "THIS IS A TEST" from opengraph tags

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,9 +7,9 @@ export class AppController {
   root() {
     return {
       message: 'Hello world!',
-      ogTitle: 'Lazztech Hub - Social App - THIS IS A TEST',
-      ogDescription: 'From app to the real world - Foster Community - THIS IS A TEST',
-      ogImage: 'https://hub.lazz.tech/assets/banner.jpg - THIS IS A TEST',
+      ogTitle: 'Lazztech Hub - Social App',
+      ogDescription: 'From app to the real world - Foster Community',
+      ogImage: 'https://hub.lazz.tech/assets/banner.jpg',
     };
   }
 }
