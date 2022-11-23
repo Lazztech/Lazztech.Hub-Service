0d98c6f2611251741f76f057f8f9392127c8a52e
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-22 18:04:32 -0800

    wip watermark file controller method

--- src/file/controller/file.controller.ts
+++ src/file/controller/file.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get, Header, Inject, Logger, NotFoundException, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { readFile } from 'fs/promises';
+import { join, resolve } from 'path';
+import sharp from 'sharp';
 import { FILE_SERVICE } from '../file-service.token';
+import { ImageFileService } from '../image-file/image-file.service';
 import { FileServiceInterface } from '../interfaces/file-service.interface';
 
 @Controller('file')
@@ -10,6 +14,7 @@ export class FileController {
   constructor(
     @Inject(FILE_SERVICE)
     private readonly fileService: FileServiceInterface,
+    private readonly imageService: ImageFileService,
   ) {}
 
   @Get(':fileName')
@@ -19,5 +24,25 @@ export class FileController {
       this.logger.error(err);
       response.status(500).send(err);
     }).pipe(response);
+  }
+
+  @Get('watermark/:fileName')
+  @Header('Cache-Control', 'public, max-age=86400') // public for CDN, max-age= 24hrs in seconds
+  @Header('content-type', 'image/jpeg')
+  async watermark(@Param('fileName') fileName: string, @Res() response: Response) {
+    const watermark = await sharp(
+      join(process.cwd(), 'public', 'assets', 'lazztech_icon.png')
+    ).resize(150, 150).toBuffer();
+    this.fileService.get(fileName).pipe(
+      sharp()
+        .jpeg()
+        .resize(1080, 1080, { fit: sharp.fit.inside })
+        .composite([
+          { input: watermark, left: 50, top: 50 },
+        ])
+    ).pipe(response).on('error', (err) => {
+      this.logger.error(err);
+      response.status(500).send(err);
+    });
   }
 }
