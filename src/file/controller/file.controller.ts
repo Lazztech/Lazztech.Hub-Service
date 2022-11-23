8d51229ad5cd0fbf767ffd10906ea555a04bc9cd
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-22 18:31:11 -0800

    Watermark (#52)
    * wip watermark file controller method
    
    * watermark with opacity
    
    * added padding to watermark
    
    * open graph service now serves watermarked jpeg url for image tags

--- src/file/controller/file.controller.ts
+++ src/file/controller/file.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get, Header, Inject, Logger, NotFoundException, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { readFile } from 'fs/promises';
+import { join, resolve } from 'path';
+import sharp from 'sharp';
 import { FILE_SERVICE } from '../file-service.token';
+import { ImageFileService } from '../image-file/image-file.service';
 import { FileServiceInterface } from '../interfaces/file-service.interface';
 
 @Controller('file')
@@ -10,6 +14,7 @@ export class FileController {
   constructor(
     @Inject(FILE_SERVICE)
     private readonly fileService: FileServiceInterface,
+    private readonly imageService: ImageFileService,
   ) {}
 
   @Get(':fileName')
@@ -19,5 +24,44 @@ export class FileController {
       this.logger.error(err);
       response.status(500).send(err);
     }).pipe(response);
+  }
+
+  @Get('watermark/:fileName')
+  @Header('Cache-Control', 'public, max-age=86400') // public for CDN, max-age= 24hrs in seconds
+  @Header('content-type', 'image/jpeg')
+  async watermark(@Param('fileName') fileName: string, @Res() response: Response) {
+    const watermark = await sharp(
+      join(process.cwd(), 'public', 'assets', 'lazztech_icon.png')
+    ).resize(150, 150)
+    .extend({
+      top: 0,
+      bottom: 20,
+      left: 20,
+      right: 0,
+      background: { r: 0, g: 0, b: 0, alpha: 0 }
+    })
+    .composite([
+      {
+        input: Buffer.from([0,0,0,200]),
+        raw: {
+          width: 1,
+          height: 1,
+          channels: 4,
+        },
+        tile: true,
+        blend: 'dest-in',
+      }
+    ]).toBuffer();
+    this.fileService.get(fileName).pipe(
+      sharp()
+        .jpeg()
+        .resize(1080, 1080, { fit: sharp.fit.inside })
+        .composite([
+          { input: watermark, gravity: 'southwest' },
+        ])
+    ).pipe(response).on('error', (err) => {
+      this.logger.error(err);
+      response.status(500).send(err);
+    });
   }
 }
