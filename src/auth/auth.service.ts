f6f1d3ef4258321b415591d2c6f77733d8dae2d2
Gian Lazzarini <gianlazzarini@gmail.com>
2022-12-06 20:12:06 -0800

    generatePassword

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,6 +12,7 @@ import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/core';
 import { ExpeditedRegistration } from './dto/expeditedRegistration.dto';
 import { generateUsername } from "unique-username-generator";
+import * as crypto from "crypto";
 
 @Injectable()
 export class AuthService {
@@ -47,7 +48,22 @@ export class AuthService {
   }
 
   generatePassword(): string {
-    return '';
+    const PASSWORD_LENGTH = 18;
+    const LOWERCASE_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'; // 26 chars
+    const UPPERCASE_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // 26 chars
+    const NUMBERS = '0123456789'; // 10 chars
+    const SYMBOLS = ',./<>?;\'":[]\\|}{=-_+`~!@#$%^&*()'; // 32 chars
+    const ALPHANUMERIC_CHARS = LOWERCASE_ALPHABET + UPPERCASE_ALPHABET + NUMBERS; // 62 chars
+    const ALL_CHARS = ALPHANUMERIC_CHARS + SYMBOLS; // 94 chars
+
+    const randomBytes = crypto.randomBytes(length);
+    let password = "";
+
+    for (var i = 0; i < PASSWORD_LENGTH; i++) {
+        randomBytes[i] = randomBytes[i] % ALL_CHARS.length;
+        password += ALL_CHARS[randomBytes[i]];
+    }
+    return password;
   }
 
   async register(
