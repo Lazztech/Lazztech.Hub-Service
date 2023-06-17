9247c298644888b16993c64c7db5c6b0042e4b76
Gian Lazzarini <gianlazzarini@gmail.com>
2023-06-17 13:46:02 -0700

    WIP hub/event and file join tables

---
+++ src/dal/entity/joinEventFile.entity.ts
@@ -0,0 +1,48 @@
+import { Entity, IdentifiedReference, ManyToOne } from '@mikro-orm/core';
+import { ObjectType } from '@nestjs/graphql';
+import { Event } from './event.entity';
+import { File } from './file.entity';
+import { User } from './user.entity';
+
+ /* eslint-disable */ // needed for mikroorm default value & type which conflicts with typescript-eslint/no-unused-vars
+@ObjectType()
+@Entity()
+export class JoinEventFile {
+
+  /**
+   * Exposed as a field resolver
+   */
+  @ManyToOne({
+    entity: () => File,
+    fieldName: 'fileId',
+    onDelete: 'cascade', 
+    primary: true,
+    wrappedReference: true
+  })
+  public file!: IdentifiedReference<File>;
+
+  /**
+   * Exposed as a field resolver
+   */
+  @ManyToOne({ 
+    entity: () => Event,
+    fieldName: 'eventId',
+    onDelete: 'cascade',
+    primary: true,
+    wrappedReference: true
+  })
+  public event!: IdentifiedReference<Event>;
+
+  /**
+   * Exposed as a field resolver
+   */
+  @ManyToOne({
+    entity: () => User,
+    fieldName: 'userId',
+    onDelete: 'cascade', 
+    primary: true,
+    wrappedReference: true
+  })
+  public approvedBy!: IdentifiedReference<User>;
+
+}
---
+++ src/dal/entity/joinHubFile.entity.ts
@@ -0,0 +1,48 @@
+import { Entity, IdentifiedReference, ManyToOne } from '@mikro-orm/core';
+import { ObjectType } from '@nestjs/graphql';
+import { File } from './file.entity';
+import { Hub } from './hub.entity';
+import { User } from './user.entity';
+
+ /* eslint-disable */ // needed for mikroorm default value & type which conflicts with typescript-eslint/no-unused-vars
+@ObjectType()
+@Entity()
+export class JoinHubFile {
+
+  /**
+   * Exposed as a field resolver
+   */
+  @ManyToOne({
+    entity: () => File,
+    fieldName: 'fileId',
+    onDelete: 'cascade', 
+    primary: true,
+    wrappedReference: true
+  })
+  public file!: IdentifiedReference<File>;
+
+  /**
+   * Exposed as a field resolver
+   */
+  @ManyToOne({ 
+    entity: () => Hub,
+    fieldName: 'hubId',
+    onDelete: 'cascade',
+    primary: true,
+    wrappedReference: true
+  })
+  public hub!: IdentifiedReference<Hub>;
+
+  /**
+   * Exposed as a field resolver
+   */
+  @ManyToOne({
+    entity: () => User,
+    fieldName: 'userId',
+    onDelete: 'cascade', 
+    primary: true,
+    wrappedReference: true
+  })
+  public approvedBy!: IdentifiedReference<User>;
+
+}
